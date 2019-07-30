0c906b6d001e0e0385d7db8b38f98c3939b0b34a
cheng chen <chencheng1q88@gmail.com>
2019-07-30 14:27:39 +1200

    Roman number converter

---
+++ RomanNumralsEncoder.js
@@ -0,0 +1,14 @@
+//Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+var convertToRoman = function(num){
+	var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+	var romanNumeral = ['M', 'CM', 'D', 'CD', 'C', 'XC','L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+	var romanized = '';
+
+	for (var index = 0; index < decimalValue.length; index++) {
+		while(decimalValue[index] <= num){
+			romanized += romanNumeral[index];
+			num -= decimalValue[index];
+		}
+	}
+}
